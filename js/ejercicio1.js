2217ecbda25b4f80b20419a06515d23c7c25623e
Rodolfo Ugarte <34588024@fi.unju.edu.ar>
2025-04-17 01:56:51 -0300

    se sube ejercicio 1

---
+++ js/ejercicio1.js
@@ -0,0 +1,7 @@
+const integrantes = "Fredy, Anahi, Rodolfo, Francisco, Lucas";
+
+const mostrarMensaje = () => {
+  console.log(`Somos ${integrantes} y estamos aprendiendo JavaScript.`);
+};
+
+mostrarMensaje();
