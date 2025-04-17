0c145663cbcfae7b89702c171f891434232b2d76
FredyDsd <fredymauro513@gmail.com>
2025-04-17 10:55:09 -0300

    si subieron ejersicios 3,4,5 y 6

---
+++ js/ejercicio4.js
@@ -0,0 +1,9 @@
+let lado = 5;
+
+let area = 2 * lado ;
+
+let perimetro = lado + lado + lado + lado;
+
+console.log(" lado del cuadrado: " + lado );
+console.log("area : " + area);
+console.log("perimetro : " + perimetro );
