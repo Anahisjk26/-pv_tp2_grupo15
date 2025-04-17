47ec8017a4f8cdabae213928ba350d74187043a3
FredyDsd <fredymauro513@gmail.com>
2025-04-17 14:27:08 -0300

    verificacion

--- js/ejersicio20.js
+++ js/ejersicio20.js
@@ -1,27 +1,4 @@
-const $form = document.querySelector('form');
-$form.addEventListener ( 'submit', function(e){
-    e.preventDefault();
-    return false;
-})
+import { obtenerPaises, actualizarCapitales } from "./../ej20/functions.js";
+import { inicializarFormulario } from "./../ej20/formulario20.js";
 
-//------------------------
-const paises = ['Buenos Aires', 'Brasilia', 'Santiago de chile', 'La Paz', 'Montevideo', 'Caracas' ];
-
-const devolverCap = document.getElementById('pais');
-
-
-paises.addEventListener("change", function () {
-
-capitales.innerHTML = '<option value="">--Selecciona una capital--</option>';
-if (paisSeleccionado) {
-    capitales.disabled = false;
-    const capital = data[paisSeleccionado];
-    const option = document.createElement("option");
-    option.value = capital.toLowerCase(); 
-    option.textContent = capital; 
-    capitales.appendChild(option); 
-    console.log(`País seleccionado: ${paisSeleccionado.toUpperCase()}`);
-    console.log(`Capital seleccionada: ${capital}`);
-} else {
-    capitales.disabled = true;
-}
+inicializarFormulario();
